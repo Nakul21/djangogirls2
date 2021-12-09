68e5aad4f7b81a9af0d96fe0c8fbeff22a6a3dae
Nakul Asthana <nakulasthana_prabhat@srmuniv.edu.in>
2021-12-09 13:12:31 +0530

    Made new migrations

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
 
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateField(default=timezone.now())
+    created_date = models.DateField(auto_now_add=True)
     published_date = models.DateField(blank=True, null=True)
 
     def publish(self):
